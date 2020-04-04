3a7145367d1e0e0f2ebc118cce625f0677b8339d
Beeant <huangxb0512@gmail.com>
2020-04-04 22:00:16 +0800

    Update leduo.py

--- leduo.py
+++ leduo.py
@@ -5,13 +5,21 @@ import requests
 import threading
 from lxml import etree
 import os
+
 from tqdm import trange, tqdm
+import time
 
 # 乐多网站网址
 url = 'http://leduomh.com/book/'
 
+# 跳过连载中的
+skip_writeing = 1
+
 # 当前目录，用于存储下载下来的图片
 folder = os.getcwd()
+
+# error log
+error_log = folder + "/error.log"
 
 # 请求头
 header = {
@@ -35,28 +43,35 @@ def getChapterImgs(book_name, path, url, chapter_name):
     chapter_path = os.path.join(path, chapter_name)
     if not os.path.exists(chapter_path):
         os.mkdir(chapter_path)
-    response = requests.get(url,
+    try:
+        response = requests.get(url,
                             headers=header,
                             proxies=proxies,
                             timeout=60
                             )
-    tree = etree.HTML(response.text)
-    imgs = tree.xpath(u"//img")
-    description = book_name + " >> " + chapter_name
-    with tqdm(total=len(imgs), desc=description) as pbar:
-        for img in imgs:
-            pbar.update(1)
-            src = img.attrib.get('data-original')
-            # 存储
-            if src:
-                p = src.split('/')
-            image_file_name = chapter_path + '/' + p[len(p) - 1]
-            if not os.path.exists(image_file_name):
-                r = requests.get(src, headers=header, proxies=proxies, timeout=60)
-                file_store(image_file_name, r)
+        tree = etree.HTML(response.text)
+        imgs = tree.xpath(u"//img")
+        description = book_name + " >> " + chapter_name
+        with tqdm(total=len(imgs), desc=description) as pbar:
+            for img in imgs:
+                pbar.update(1)
+                src = img.attrib.get('data-original')
+                # 存储
+                if src:
+                    p = src.split('/')
+                image_file_name = chapter_path + '/' + p[len(p) - 1]
+                if not os.path.exists(image_file_name):
+                    r = requests.get(src, headers=header, proxies=proxies, timeout=60)
+                    file_store(image_file_name, r)
+        time.sleep(1)
+        
+    except Exception as e:
+        print(book_name + " " + chapter_name + " 获取异常")
+        log_error(book_name + " " + chapter_name + " 获取异常")
 
-        # print("####结束下载：" + book_name + ' >> '+ chapter_name)
-
+def log_error(msg):
+    with open(error_log, 'a+') as f:
+        f.write( time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + " " + msg + '\n')
 
 def cleanName(name):
     return name.replace('/', '-').replace(':', '-').replace("：", "-")
@@ -64,34 +79,41 @@ def cleanName(name):
 
 # 获取章节信息
 def getChapter(url, num):
-    response = requests.get(url, headers=header, proxies=proxies, timeout=60)
-    tree = etree.HTML(response.text)
+    try:
+        response = requests.get(url, headers=header, proxies=proxies, timeout=60)
+        tree = etree.HTML(response.text)
 
-    # 名称
-    book = tree.xpath("//div[@class='banner_detail_form']//div[@class='info']//h1")
-    if (book):
-        book_name = cleanName(book[0].text)
-        book_path = folder + "/" + str(num) + "_" + book_name
-        if not os.path.exists(book_path):
-            os.mkdir(book_path)
-        # 寻找章节
-        chapters = tree.xpath("//ul[@id='detail-list-select']//li//a")
-        # 限制最大20个线程池
-        threads = [0] * len(chapters)
-        # thread_len = 5
-        # thread_pool = ThreadPoolExecutor(thread_len)
+        # 名称
+        book = tree.xpath("//div[@class='banner_detail_form']//div[@class='info']//h1")
+        status = tree.xpath("//div[@class='banner_detail_form']//div[@class='info']/p[position()=3]/span/span")[0].text
+        if (book):
+            if skip_writeing == 1 and status == '连载中':
+                return ;
+            book_name = cleanName(book[0].text) + "-" + status
+            book_path = folder + "/" + str(num) + "_" + book_name
+            if not os.path.exists(book_path):
+                os.mkdir(book_path)
+            # 寻找章节
+            chapters = tree.xpath("//ul[@id='detail-list-select']//li//a")
+            # 限制最大20个线程池
+            threads = [0] * len(chapters)
+            # thread_len = 5
+            # thread_pool = ThreadPoolExecutor(thread_len)
 
-        # threads = [0] * thread_len
-        with trange(len(chapters), desc=book_name) as pbar:
-            for k, chapter in enumerate(chapters):
-                # tchapters.set_description(description + " %s")
-                href = chapter.attrib.get('href')
-                name = cleanName(chapter.text)
-                threads[k] = myThread(book_name, book_path, 'http://leduomh.com' + href, name, pbar)
-                threads[k].start()
-                # thread_pool.submit(getChapterImgs, boock_name, book_path, 'http://leduomh.com' + href, name)
-            for k, thread in enumerate(threads):
-                thread.join()
+            # threads = [0] * thread_len
+            with trange(len(chapters), desc=book_name) as pbar:
+                for k, chapter in enumerate(chapters):
+                    # tchapters.set_description(description + " %s")
+                    href = chapter.attrib.get('href')
+                    name = cleanName(chapter.text)
+                    threads[k] = myThread(book_name, book_path, 'http://leduomh.com' + href, name, pbar)
+                    threads[k].start()
+                    # thread_pool.submit(getChapterImgs, boock_name, book_path, 'http://leduomh.com' + href, name)
+                for k, thread in enumerate(threads):
+                    thread.join()
+    except Exception as e:
+        print(url + " 访问异常")
+        log_error(url + " 访问异常")
 
 
 # 定义线程加速爬
@@ -112,9 +134,10 @@ class myThread(threading.Thread):
 
 def main():
     # 目前知道的只有4 到 662 之间的数字有漫画，后续可自行定义
-    for num in range(10, 662):
+    for num in range(4, 662):
         getChapter(url + str(num), num)
-
+        time.sleep(2)
+        
 
 if __name__ == '__main__':
     main()
